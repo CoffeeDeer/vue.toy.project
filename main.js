a12028cd56976b04243a56732920d66fbdbc3807
김현기 <11609005+rubi1541@users.noreply.github.com>
2019-06-24 23:45:15 +0900

    express - pug test

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var path = require('path');
+//var routes = require('./routes/index');
 
-var app = http.createServer(function(req,res){
-    var url = req.url;
+var app = express();
 
-    res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'} );
-    //console.log(url);
-    res.end(fs.readFileSync('.'+url));
+// view engine setup
+app.set('views', path.join(__dirname, 'static'));
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+    res.render('test',
+        {
+            title: 'Hey',
+            message: 'Hello there!'
+        });
 });
 
-app.listen(80);
+app.listen(80, () => {
+    console.log('Express App on port 80!');
+});
+
+
+//app.use('/', routes);
